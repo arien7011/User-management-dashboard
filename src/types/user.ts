5dd2c965b683d095335a88c0953abf2aebeef5b8
amanmishra-tz <aman.mishra@taazaa.com>
2025-11-21 18:15:58 +0530

    feat: add user management  dashboard initial setup structure

---
+++ src/types/user.ts
@@ -0,0 +1,9 @@
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  company: { name: string };
+  address: { street: string; city: string; zipcode: string };
+
+}
